364c7aea9a7bd52a671b5f56e01bf18462b06bff
Datson-bit <datonayomide@gmail.com>
2025-01-26 00:53:32 +0100

    first commit

---
+++ employee/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Employee(models.Model):
+    employee_id = models.CharField(max_length=20)
+    employee_name = models.CharField(max_length=100)
+    employee_email = models.EmailField()
+    employee_contact = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.employee_name
